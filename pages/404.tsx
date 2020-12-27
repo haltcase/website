57b2f8aa30be018a26a40d92e1b2eadc8ac71436
citycide <lingenbw@gmail.com>
2020-12-26 18:42:33 -0600

    add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Link from "next/link"
+
+import Content from "../components/Content"
+import Layout from "../components/Layout"
+import Main from "../components/Main"
+
+import styles from "./404.module.css"
+
+const NotFoundPage: React.FunctionComponent = () => (
+  <Layout title="Bo Lingen">
+    <Main>
+      <Content
+        header="404"
+        className={styles.content}
+        headerClassName={styles.header}>
+        <p className={styles.errorBody}>
+          This page could not be found.
+          <Link href="/">
+            <a className={styles.returnButton}><button>Home</button></a>
+          </Link>
+        </p>
+      </Content>
+    </Main>
+  </Layout>
+)
+
+export default NotFoundPage
