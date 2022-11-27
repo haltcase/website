7a69ecf2db3cb62b3a562638f30f81994138ad99
haltcase <bo@lingen.life>
2022-11-26 18:39:49 -0600

    add plausible for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import "./styles.css";
 
 import { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 import { FunctionComponent } from "react";
 
+const domain =
+	process.env.NODE_ENV === "production" ? "bolingen.me" : "localhost:3000";
+
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<PlausibleProvider
+			domain={domain}
+			customDomain="https://plausible.lab.lingen.life"
+			selfHosted
+			enabled={domain.includes("localhost") || undefined}
+			trackLocalhost={domain.includes("localhost")}
+			trackOutboundLinks>
+			<Component {...pageProps} />
+		</PlausibleProvider>
+	);
 };
 
 export default App;
