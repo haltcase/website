e31ce6d617626c4438ae8ee22d90e2784cd5079c
haltcase <bo@lingen.life>
2022-11-26 18:49:23 -0600

    add eslint next plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:react/recommended",
 		"plugin:react/jsx-runtime",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@next/next/recommended"
 	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
