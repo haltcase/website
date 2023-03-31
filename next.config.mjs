aa671a65ac0f6f0db22bc86372a8ceffd4a07728
haltcase <bo@lingen.life>
2023-03-30 19:39:00 -0500

    fix next redirects definition

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 import { withPlausibleProxy } from "next-plausible";
 
-export default withPlausibleProxy({
-	customDomain: "https://plausible.lab.lingen.life"
-})({
-	redirects: [
+/**
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = {
+	redirects: async () => [
 		{
-			basePath: false,
-			source: "/.well-known/webfinger",
+			source: "/.well-known/webfinger:path*",
 			destination:
-				"https://hachyderm.io/.well-known/webfinger?resource=acct:haltcase@hachyderm.io",
-			permanent: true
+				"https://hachyderm.io/.well-known/webfinger?resource=acct:haltcase@hachyderm.io"
 		}
 	]
-});
+};
+
+export default withPlausibleProxy({
+	customDomain: "https://plausible.lab.lingen.life"
+})(nextConfig);
