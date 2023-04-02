2595d1171d382c61624b454c738518242f2aba5a
Bo Lingen <bo@lingen.life>
2023-04-02 17:29:51 -0500

    use umami for analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,10 @@
 import "./styles.css";
 
 import { AppProps } from "next/app";
-import PlausibleProvider from "next-plausible";
 import { FunctionComponent } from "react";
 
-const domain =
-	process.env.NODE_ENV === "production" ? "bolingen.me" : "localhost:3000";
-
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-	return (
-		<PlausibleProvider
-			domain={domain}
-			customDomain="https://plausible.lab.lingen.life"
-			selfHosted
-			enabled={domain.includes("localhost") || undefined}
-			trackLocalhost={domain.includes("localhost")}
-			trackOutboundLinks>
-			<Component {...pageProps} />
-		</PlausibleProvider>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default App;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+	return (
+		<Html>
+			<Head>
+				<script
+					async
+					defer
+					data-website-id="ebb6ad49-b14c-443e-8ae1-38aebeac7aaf"
+					src="/stats/script.js"></script>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
