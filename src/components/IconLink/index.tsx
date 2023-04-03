e995b42c8f62b5f78be7d19dccf47c6863dcd204
Bo Lingen <bo@lingen.life>
2023-04-02 21:09:04 -0500

    support additional classes on IconLink

--- src/components/IconLink/index.tsx
+++ src/components/IconLink/index.tsx
@@ -9,6 +9,7 @@ interface Props {
 	size?: string;
 	rel?: string;
 	target?: string;
+	className?: string;
 }
 
 const IconLink: FunctionComponent<Props> = ({
@@ -17,9 +18,15 @@ const IconLink: FunctionComponent<Props> = ({
 	title = "",
 	size = "18",
 	rel = "noreferrer",
-	target = "_blank"
+	target = "_blank",
+	className
 }) => (
-	<a href={href} className="icon" title={title} rel={rel} target={target}>
+	<a
+		className={`icon ${className}`}
+		href={href}
+		title={title}
+		rel={rel}
+		target={target}>
 		<Icon size={size} />
 	</a>
 );
